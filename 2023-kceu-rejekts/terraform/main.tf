f202566b3317a8c2620836795df58951d9362b4e
Thomas Schuetz <thomas.schuetz@t-sc.eu>
2023-04-15 15:51:18 +0200

    feat: added example app
    Signed-off-by: Thomas Schuetz <thomas.schuetz@t-sc.eu>

---
+++ 2023-kceu-rejekts/terraform/main.tf
@@ -0,0 +1,15 @@
+module "demo-cluster" {
+  source = "github.com/on-clouds/terraform"
+  argo_enabled     = true
+  argo_envs        = [ {
+    repoUrl = "https://github.com/on-clouds/demos"
+    path    = "2023-kceu-rejekts/argo"
+    name    = "2023-kceu-rejekts"
+  }]
+  cluster_location = "us-central1-a"
+  cluster_name     = "kceu-demo-cluster"
+  keptn_enabled    = true
+  project_id       = "thschue-demos"
+}
+
+
