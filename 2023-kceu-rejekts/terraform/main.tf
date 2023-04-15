f2b887ac2f36d86de46f3a52056e56cfc8e44e6d
Thomas Schuetz <thomas.schuetz@t-sc.eu>
2023-04-15 17:50:02 +0200

    keptn fixed pre-deployment
    Signed-off-by: Thomas Schuetz <thomas.schuetz@t-sc.eu>

--- 2023-kceu-rejekts/terraform/main.tf
+++ 2023-kceu-rejekts/terraform/main.tf
@@ -2,13 +2,16 @@ variable "project_id" {
 }
 
 module "demo-cluster" {
-  source = "github.com/on-clouds/terraform"
-  argo_enabled     = true
-  argo_envs        = [ {
-    repoUrl = "https://github.com/on-clouds/demos"
-    path    = "2023-kceu-rejekts/argo"
-    name    = "2023-kceu-rejekts"
-  }]
+  source       = "github.com/on-clouds/terraform"
+  argo_enabled = true
+  cm_enabled   = true
+  argo_envs    = [
+    {
+      repoUrl = "https://github.com/on-clouds/demos"
+      path    = "2023-kceu-rejekts/argo"
+      name    = "2023-kceu-rejekts"
+    }
+  ]
   cluster_location = "us-central1-a"
   cluster_name     = "kceu-demo-cluster"
   keptn_enabled    = true
